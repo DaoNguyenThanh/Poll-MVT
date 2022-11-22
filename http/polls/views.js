4a0d44555fb5213a715add87a84eff69d2e29bfa
DaoNT8952 <nguyenthanhdao1619@gmail.com>
2022-11-22 17:40:37 +0700

    customizing form after submit

--- http/polls/views.js
+++ http/polls/views.js
@@ -1,30 +1,28 @@
 const models = require(require('path').resolve('./') + '/models');
  
-exports.index = async (req, res, next) => {
-    res.render('polls/index');
-}
-
 exports.new = async (req, res, next) => {
-    // Query lay danh sach cac cuoc khao sat
-    // const polls = await models.Poll.findMany({
-    // // const myJSON = await models.Poll.findMany({
-    //     include: {
-    //         questions:{   
-    //             select: { 
-    //                 name:  true,
-    //                 type: true, 
-    //                 answers: {
-    //                     select: {
-    //                         name: true
-    //                     }
-    //                 }
-    //             } 
-    //         }
-    //     }
-    // })
     res.render('polls/new');
 }
 
+exports.index = async (req, res, next) => {
+    //Query lay danh sach cac cuoc khao sat
+    const polls = await models.Poll.findMany({
+        include: {
+            questions:{   
+                select: { 
+                    name:  true,
+                    type: true, 
+                    answers: {
+                        select: {
+                            name: true
+                        }
+                    }
+                } 
+            }
+        }
+    })
+    res.render('polls/index', { polls });
+}
 //creating form
 exports.create = async (req, res, next) => {
     let answerNameData = [];
@@ -33,19 +31,19 @@ exports.create = async (req, res, next) => {
             name: req.body.answer_name[i]
         })
     }
-    // await models.Poll.create({
-    //     data: {
-    //         name: req.body.poll_name,   
-    //             questions: { 
-    //                 create: {
-    //                     name:  req.body.question_name,
-    //                     type:  req.body.question_type,
-    //                     answers: {
-    //                         create: answerNameData
-    //                     },
-    //                 }
-    //             }   
-    //     }
-    // })
+    await models.Poll.create({
+        data: {
+            name: req.body.poll_name,   
+                questions: { 
+                    create: {
+                        name:  req.body.question_name,
+                        type:  req.body.question_type,
+                        answers: {
+                            create: answerNameData
+                        },
+                    }
+                }   
+        }
+    })
     res.redirect('/polls');
 }
