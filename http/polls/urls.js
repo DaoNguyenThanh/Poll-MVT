73259415b66dd39f66a00686f6345f5fb971b99a
Dao Nguyen Thanh <nguyenthanhdao1619@gmail.com>
2022-11-28 22:44:19 +0700

    voting complete

--- http/polls/urls.js
+++ http/polls/urls.js
@@ -9,8 +9,6 @@ path.route('/new').get(views.new);
 
 path.route('/:poll_id/vote').post(views.vote);
 
-path.route('/:poll_id/vote').get(views.count);
-
 //path.route('/:poll_id/questions/:questions_id/answers/:answers_id/vote').post(views.vote);
 //path.route('/:answer_id/vote/:user_id').get(views.count);
 
--- http/polls/views.js
+++ http/polls/views.js
@@ -7,7 +7,7 @@ exports.new = async (req, res, next) => {
 exports.index = async (req, res, next) => {
     //Query lay danh sach cac cuoc khao sat
     const polls = await models.Poll.findMany({
-        include: {
+        select: {
             questions:{   
                 select: {
                     id: true,
@@ -16,10 +16,15 @@ exports.index = async (req, res, next) => {
                     answers: {
                         select: {
                             id: true,
-                            name: true
+                            name: true,
+                            _count: {
+                                select: {
+                                    users: true
+                                }
+                            }
                         }
                     }
-                } 
+                },
             }
         }
     })
@@ -85,58 +90,3 @@ exports.vote = async (req, res, next) => {
   res.redirect('/polls');
 }
 
-exports.count = async (req, res, next) => {
-    
-    // const count_voting = await models.Result.findMany({
-    //     where: { slug },
-    //     include: {
-    //         _count: {
-    //           select: { 
-    //             user_id : true,
-    //             answer_id: true
-    //           }
-    //         }
-    //     }
-    // })
-    const idofanswer = await models.Answer.findUnique({
-        where: { slug },
-        include: {
-            users: {
-              include: {
-                results: {
-                  where: { Answer: { slug } },
-                }
-              }
-            }
-        }
-    })
-
-    const idAnswer = {
-        ...idofanswer,
-        results: idofanswer?.users.map(({ results, ...rest }) => ({
-            ...rest,
-            _count: { results: results.length },
-        })),
-    };
-    res.redirect('/polls', {idAnswer});
-  //   const _battle = await prisma.battle.findUnique({
-  //     where: { slug },
-  //     include: {
-  //         fighters: {
-  //             include: {
-  //                 votes: {
-  //                     where: { Battle: { slug } },
-  //                 },
-  //             },
-  //         },
-  //     },
-  // });
-  
-  // const battle = {
-  //     ..._battle,
-  //     fighters: _battle?.fighters.map(({ votes, ...rest }) => ({
-  //         ...rest,
-  //         _count: { votes: votes.length },
-  //     })),
-  // };
-}
