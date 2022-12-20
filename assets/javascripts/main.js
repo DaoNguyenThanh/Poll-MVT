34de50cdf6c4cb2bb693ad0ddfa3c488308a2552
DaoNT8952 <nguyenthanhdao1619@gmail.com>
2022-12-20 17:54:20 +0700

    add progress bar in poll

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -44,4 +44,31 @@ function addRow() {
   signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
   });
-//POLL WEBSITE
+//progress bar
+// var i = 0;
+// function move() {
+//   if (i == 0) {
+//     i = 1;
+//     var elem = document.getElementById("myBar");
+//     var width = 10;
+//     var id = setInterval(frame, 10);
+//     function frame() {
+//       if (width >= 100) {
+//         clearInterval(id);
+//         i = 0;
+//       } else {
+//         width++;
+//         elem.style.width = width + "%";
+//         elem.innerHTML = width  + "%";
+//       }
+//     }
+//   }
+// }
+
+const progress_bar = document.getElementsByClassName("myBar");
+
+progress_bar.addEventListener('click',()=> {
+  document.getElementsByClassName("vote-count") = 0 ;
+  if document.getElementsByClassName("vote-count") > 1
+});
+
