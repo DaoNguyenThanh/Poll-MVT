3254fe5b71b451f7fc0f74a8baaaefed6490ea52
DaoNT8952 <nguyenthanhdao1619@gmail.com>
2022-12-22 17:59:41 +0700

    add progress bar in poll part 4

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -65,11 +65,49 @@ function addRow() {
 //   }
 // }
 
-const progress_bar = document.getElementsByClassName("myBar");
+// const progress_bar = document.getElementsByClassName("myBar");
 
-// progress_bar.addEventListener('click',()=> {
-//   document.getElementsByClassName("vote-count") = 0 ;
-//   if document.getElementsByClassName("vote-count") > 1
+let progress = document.querySelector(".vote-btn");
 
-// });
+let i = 0;
+progress.forEach( function() {
 
+    progress[i].addEventListener( "click", function() {
+
+        // let getSelection = this.getAttribute("name");
+        // let setSelection = document.querySelector("." + getSelection);
+        // let getCounter = document.querySelector("." + getSelection).textContent;
+        let getSelection = this.getAttribute("name");
+        let setSelection = document.querySelector("." + getSelection);
+        let getCounter = document.querySelector("." + getSelection + "-counter").textContent;
+        let setCounter = Number.parseInt(getCounter) + 1;
+
+        let currentWidth = setSelection.style.width;
+        let newWidth = Number.parseInt(currentWidth) + 20;
+
+        setSelection.classList.add("running");
+        document.querySelector("#container").classList.add("running");
+
+        setTimeout(() => {
+            setSelection.classList.remove("running");
+            document.querySelector("#container").classList.add("running");
+        }, 1000);
+
+        if ( isNaN(newWidth) ) {
+            setSelection.style.width = "20%";
+        } else {
+            if ( newWidth <= 100) {
+                setSelection.style.width = newWidth + "%";
+            } else {
+                setSelection.style.width = "0%";
+                setCounter = 0;
+            }
+        };
+
+        setSelection.getAttribute("style");
+        document.querySelector("." + getSelection + "-counter").textContent = setCounter;
+    })
+
+    ++i;
+
+})
